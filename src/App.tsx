70c50a5d7c95e965f7f7bd1e92e1ba900da43e37
Chase Allen <ccallen001@gmail.com>
2021-02-23 18:11:40 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App" data-testid="App">
+      <header className="App-header">
+        ccallen001.github.io
+      </header>
+    </div>
+  );
+}
+
+export default App;

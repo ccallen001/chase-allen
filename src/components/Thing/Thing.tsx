30a39398de96d6a7a5155bfd2cf15f68746befb1
Chase Allen <ccallen001@gmail.com>
2021-02-28 04:10:49 -0500

    some content and styling

---
+++ src/components/Thing/Thing.tsx
@@ -0,0 +1,7 @@
+import './Things.scss';
+
+function Thing() {
+  return <div className="component Thing">{'> What?'}</div>;
+}
+
+export default Thing;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,12 @@
+import Thing from '../../components/Thing/Thing';
+
 import './Home.scss';
 
 function Home() {
   return (
     <div className="view Home">
       Home
+      <Thing />
     </div>
   );
 }

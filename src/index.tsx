fea25d5ae1702aa29f27795da03c4efef6a23f6c
Chase Allen <ccallen001@gmail.com>
2021-03-05 19:51:27 -0500

    use HashRouter because of github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 
 import './index.scss';
 
@@ -9,9 +9,9 @@ import App from './app/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
